50b5187a1e61bb9ff3c60bfeda3a0b0ee1c25e64
mongarerobert3 <mongarerobert3@gmail.com>
2023-05-29 17:48:24 +0300

    travel

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,35 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { NavbarDashboard, HeroDashboard, Footer } from '../../components';
-import { fetchTrips } from './dashboard';
-
 import './index.css';
 
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const options = { day: 'numeric', month: 'short', year: 'numeric' };
-  const formatter = new Intl.DateTimeFormat('en-US', options);
-  const formattedDate = formatter.format(date);
-  return formattedDate;
-}
+import { NavbarDashboard, HeroDashboard, Footer } from '../../components';
+import { formatDate, fetchTrips, prevSlide, nextSlide } from './dashboard';
 
 const Dashboard = () => {
   const [trips, setTrips] = useState([]);
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    fetchTrips(token)
-      .then(data => setTrips(data));
-  }, [token]);
-
-  useEffect(() => {
-    const sliderInterval = setInterval(() => {
-      setCurrentSlide(prevSlide => (prevSlide + 1) % trips.length);
-    }, 5000);
-
-    return () => clearInterval(sliderInterval);
-  }, [trips]);
+    fetchTrips(setTrips, token);
+  }, []);
 
   return (
     <div>
@@ -39,31 +22,36 @@ const Dashboard = () => {
         <div className="row">
           <div className="col-9">
             <div className="dashboard-tour-card-container">
-              <div className="trip-cards-slider">
-                <div className="trip-cards-wrapper">
-                  {trips.map((trip, index) => (
-                    <div
-                      key={trip.id}
-                      className={`dashboard-tour-card ${index === currentSlide ? 'active' : ''}`}
-                    >
+              <div className="trip-cards-slider" style={{ transform: `translateX(-${currentIndex * 25}%)` }}>
+                {trips.slice(currentIndex, currentIndex + 4).map((trip) => (
+                  <div key={trip.id} className="dashboard-tour-card">
+                    <div className="card-image-wrapper">
                       <img src={trip.imageCover} alt="Trip Cover" className="trip-cover-image" />
-                      <div className="card-overlay">
-                        <div className="card-overlay-content">
-                          <h4>{trip.name}</h4>
-                          <p>{formatDate(trip.startDate)} - {formatDate(trip.endDate)}</p>
-                          <p>Available Seats: {trip.availableSeats}</p>
-                        </div>
+                    </div>
+                    <div className="card-details">
+                      <h4>{trip.name}</h4>
+                      <div className="card-hover-content">
+                        <p>{formatDate(trip.startDate)} - {formatDate(trip.endDate)}</p>
+                        <p>Available Seats: {trip.availableSeats}</p>
+                        <p>Price: {trip.price}</p>
+                        <button className="book-button">Book Now</button>
                       </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
+              <button className="slider-button prev-button" onClick={() => prevSlide(currentIndex, setCurrentIndex, trips)}>
+                &#10094;
+              </button>
+              <button className="slider-button next-button" onClick={() => nextSlide(currentIndex, setCurrentIndex, trips)}>
+                &#10095;
+              </button>
             </div>
           </div>
           <div className="col-3 recommended-trips">
             <h3 className="recommended-trips-header">Upcoming trips</h3>
             <ul className="recommended-trips-list">
-              {trips.map(trip => (
+              {trips.map((trip) => (
                 <div key={trip.id} className="container-fluid mt-3">
                   <div className="row">
                     <div className="col p-3">
@@ -73,6 +61,9 @@ const Dashboard = () => {
                     </div>
                     <div className="col p-3">
                       <h4>{trip.name}</h4>
+                      <p className="dates">
+                        {formatDate(trip.startDate)} - {formatDate(trip.endDate)}
+                      </p>
                     </div>
                   </div>
                 </div>
--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,16 +1,32 @@
 import axios from 'axios';
 
-export function fetchTrips(token) {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { day: 'numeric', month: 'short', year: 'numeric' };
+  const formatter = new Intl.DateTimeFormat('en-US', options);
+  const formattedDate = formatter.format(date);
+  return formattedDate;
+}
 
-  return axios.get('http://localhost:5000/api/tour', config)
-    .then(response => response.data)
-    .catch(error => {
-      console.error('Error fetching upcoming trips:', error);
-      return [];
-    });
+export async function fetchTrips(setTrips, token) {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.get('http://localhost:5000/api/tour', config);
+    const data = response.data;
+    setTrips(data);
+  } catch (error) {
+    console.error('Error fetching upcoming trips:', error);
+  }
+}
+
+export function prevSlide(currentIndex, setCurrentIndex, trips) {
+  setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : trips.length - 1));
+}
+
+export function nextSlide(currentIndex, setCurrentIndex, trips) {
+  setCurrentIndex((prevIndex) => (prevIndex < trips.length - 1 ? prevIndex + 1 : 0));
 }
